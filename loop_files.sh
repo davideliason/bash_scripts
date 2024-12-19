80e11f380bbee68dcd751627269df4c8ba0a77e0
David Eliason <davethemaker@gmail.com>
2024-12-19 14:15:56 -0800

    within loop_files script, create hello file and danger file. The latter will be found using grep and removed

--- loop_files.sh
+++ loop_files.sh
@@ -6,4 +6,7 @@ echo "you entered $1"
 
 # cd into given arg folder and create file
 
-cd $1 && touch hello.txt
+cd $1 && touch hello.txt && touch danger.txt
+
+# now loop through folder and delete certain files using grep
+
