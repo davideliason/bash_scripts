3f0b2669bc48160c31198b1c45c15cae84737bb3
David Eliason <davethemaker@gmail.com>
2024-12-19 14:32:53 -0800

    add functionality to loop_files script that allows user to provide folder as arg and also creates a dangerous (not really) file to be acted upon by the script using find and rm

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ i##Basic Scripts
 - while.sh // simple script to create while loop to implement counter
 - check_random_if.sh // generate random number, check user input number for match
 - backup_script.sh // backsup sandbox folder and compresses it using gzip
+- loop_files.sh // user can optionally provide folder as argument, the script then places a dangerous file in that given folder and/or the default folder within this repo. The script then uses the find and rm commands to search out these dangerous files and removes them"
+
--- loop_files.sh
+++ loop_files.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #loop through files provided as command arg
 #
+#create a default danger file to be deleted in case the user does not
+touch /Desktop/vcs_projects/bash_scripts/random_source_folder/dangerous.txt
+
 echo $1
-echo "you entered $1"
+echo "you entered $1. A scary dangerous file will be made there!"
 
 # cd into given arg folder and create file
 
 cd $1 && touch hello.txt && touch danger.txt
 
 # now loop through folder and delete certain files using grep
+find ~/Desktop/vcs_projects/bash_scripts/random_source_folder f -name "*danger*" -exec rm {} \;$
 
+echo "whew! dangerous file removed!"
