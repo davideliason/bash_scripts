373cba7dc7a5135d19e5d467f8f59ae858efcd31
David Eliason <davethemaker@gmail.com>
2024-12-20 11:02:49 -0800

    create while loop checking for user input and meanwhile logging any new additions to log file

--- log_file_monitor.sh
+++ log_file_monitor.sh
@@ -6,3 +6,15 @@ filepath="${dir}/${userfile}"
 echo "The full file path is: $filepath"
 
 tail -n 10 $filepath
+
+while True
+do
+        tail -f $filename
+        read userinput
+        if [ $? -eq 0 ];
+        then 
+                exit
+        else
+                continue
+        fi
+done
