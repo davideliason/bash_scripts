bca832b3e8b979bcfbd411387077caf86eeccf0e
David Eliason <davethemaker@gmail.com>
2024-12-20 12:52:24 -0800

    refactor to try and get interrupt working

--- log_file_monitor.sh
+++ log_file_monitor.sh
@@ -8,10 +8,9 @@ echo "The full file path is: $filepath"
 tail -n 10 $filepath
 
 trap "echo 'key prressed exitingr'; exit 0" SIGINT
+# pof
+echo "now to show ongoing log additions:"
 
-while True
-do
+while true; do
         tail -f $filename
-
-        read -n 1 -s;
 done
